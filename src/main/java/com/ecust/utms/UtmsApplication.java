8bb1033f4ea29ad8b66386f254417c7d638ad7f7
Sijar <1278824488@qq.com>
2019-07-23 09:56:08 +0800

    新建的springboot框架

---
+++ src/main/java/com/ecust/utms/UtmsApplication.java
@@ -0,0 +1,13 @@
+package com.ecust.utms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtmsApplication.class, args);
+	}
+
+}
